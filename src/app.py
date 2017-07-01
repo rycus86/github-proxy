bde4642158d6ec8261da7f0418cfb8e7aa66848a
Viktor Adam <rycus86@gmail.com>
2017-07-01 22:55:51 +0100

    Exclude cli main from coverage reports

--- src/app.py
+++ src/app.py
@@ -85,7 +85,7 @@ def get_commit_stats(username, repository):
     return jsonify(api.get_commit_stats(username, repository))
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     app.run(host=os.environ.get('HTTP_HOST', '127.0.0.1'),
             port=int(os.environ.get('HTTP_PORT', '5000')),
             debug=False)
